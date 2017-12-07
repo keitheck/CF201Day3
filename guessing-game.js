1197d414c09b34e06500ce0c7d247c2f36da096a
keitheck <eckertkeith@gmail.com>
2017-12-06 23:33:30 -0800

    fixed text

--- guessing-game.js
+++ guessing-game.js
@@ -5,21 +5,21 @@ var counter = 1;
 var randomNumber = Math.floor(Math.random() * 10);
 console.log('generated number:', randomNumber);
 
-var guessedNumber = parseInt(prompt('I am thinking of a number between 1 and 10.  Can you guess it?'));
+var guessedNumber = parseInt(prompt('I am thinking of a number between 0 and 10.  Can you guess it?'));
 console.log('number guessed:', guessedNumber);
 
 while (guessedNumber !== randomNumber) {
 
   if(guessedNumber < randomNumber) {
-    guessedNumber = parseInt(prompt('Too low.\n Please guess another number between 1 and 10.'));
+    guessedNumber = parseInt(prompt('Too low.\n Please guess another number between 0 and 10.'));
     console.log('number guessed:', guessedNumber);
     counter++;
   } else if(guessedNumber > randomNumber) {
-    guessedNumber = parseInt(prompt('Too high.\n Please guess another number between 1 and 10.'));
+    guessedNumber = parseInt(prompt('Too high.\n Please guess another number between 0 and 10.'));
     console.log('number guessed:', guessedNumber);
     counter++;
   } else if(isNaN(number) || number === null) {
-    guessedNumber = parseInt(prompt('Not a number.\n Please guess another number between 1 and 10.'));
+    guessedNumber = parseInt(prompt('Not a number.\n Please guess another number between 0 and 10.'));
     console.log('number guessed:', guessedNumber);
   }
 }
